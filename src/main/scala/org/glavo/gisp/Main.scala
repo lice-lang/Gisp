bd313475ca250af0004fdec9098ebaa39cc51a7c
Glavo <zjx001202@126.com>
2017-08-12 23:50:41 +0800

    Auto commit by Glavo

---
+++ src/main/scala/org/glavo/gisp/Main.scala
@@ -0,0 +1,13 @@
+package org.glavo.gisp
+
+/**
+  * Created by Glavo on 17-8-12.
+  *
+  * @author Glavo
+  * @since 0.1.0
+  */
+object Main {
+    def main(args: Array[String]): Unit = {
+
+    }
+}
---
+++ src/main/scala/org/glavo/gisp/helper/StringUtils.scala
@@ -0,0 +1,16 @@
+package org.glavo.gisp.helper
+
+import java.io._
+/**
+  * Created by Glavo on 17-8-12.
+  *
+  * @author Glavo
+  * @since 0.1.0
+  */
+object StringUtils {
+    def escape(input: String): String = {
+
+
+        ???
+    }
+}
